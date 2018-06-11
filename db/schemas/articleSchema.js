dd513f135a5de4f4aa1856ed79e8d7c66d7bc716
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-10 22:16:00 -0400

    created article schema

--- db/schemas/articleSchema.js
+++ db/schemas/articleSchema.js
@@ -1,9 +1,11 @@
 var Schema = require('mongoose').Schema;
+const commentsSchema = require('./commentsSchema')
 
 const articleSchema = new Schema({
     link: String,
     title: String,
-    summary: String
+    summary: String,
+    comments: [ commentsSchema ]
 })
 
 module.exports = articleSchema;
