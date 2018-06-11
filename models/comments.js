658e8a7d8094c78454c898d0803affbe6a48db16
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-10 21:58:36 -0400

    created comment model

---
+++ models/comments.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const commentsSchema = require('../db/schemas/commentsSchema');
+
+const Comments = mongoose.model('comments', commentsSchema);
+
+module.exports = Comments;
