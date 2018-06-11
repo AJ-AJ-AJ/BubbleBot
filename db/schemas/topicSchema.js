4bc542d88ce415def412be4bb2513d54c17698aa
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-10 22:01:49 -0400

    created topic schema

--- db/schemas/topicSchema.js
+++ db/schemas/topicSchema.js
@@ -1,8 +1,10 @@
 var Schema = require('mongoose').Schema;
+const articleSchema = require('./articleSchema')
 
 const topicSchema = new Schema({
     name: String,
     description: String,
+    articles: [ articleSchema ]
 })
 
 module.exports = topicSchema;
