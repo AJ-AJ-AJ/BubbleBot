a78d7e0ed06f5e770ccf484ae053023856342285
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-06-10 22:23:08 -0400

    created topic controller

---
+++ controllers/topicController.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const Topic = require('../models/topic')
+
+router.get('/', (req, res) => {
+
+    // Find all Comments
+    Topic
+      .find()
+      .then((listOfTopic) => {
+  
+        // Once you have all Topic, then render out index page homeworks is all
+        // pieces of data that match the Homework Model
+        res.render('topic/index', { listOfTopic: listOfTopic })
+      })
+      .catch((err) => res.send(err))
+  
+  })
+  
+
+
+  module.exports = router;
